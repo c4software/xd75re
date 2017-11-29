47be1854dbd1281da1a29391389b6354ce38bd9d
Valentin Brosseau <c4software@gmail.com>
2017-11-29 19:12:29 +0100

    Add key to control the backlight

--- layout/c4software/qmk_firmware/keymaps/c4software/keymap.c
+++ layout/c4software/qmk_firmware/keymaps/c4software/keymap.c
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | F1     | F2     | F3     | F4     | F5     | F6     |        | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | CALC   |        |        |        |        | P7     | P8     | P9     | -      |        |        |        |        |        |
+ * |        | CALC   |        |        |        |        | P7     | P8     | P9     | -      | RGB_TOG| RGB_MOD| RGB_HUI| RGB_HUD|        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | PREV   | PLAY   | NEXT   | STOP   |        | P4     | P5     | P6     | +      |        |        |        |        |        |
+ * |        | PREV   | PLAY   | NEXT   | STOP   |        | P4     | P5     | P6     | +      | RGB_SAI| RGB_SAD| RGB_VAI| RGB_VAD|        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | VOL-   | MUTE   | VOL+   |        |        | P1     | P2     | P3     | PENT   |        |        |        |        |        |
+ * |        | VOL-   | MUTE   | VOL+   |        |        | P1     | P2     | P3     | PENT   | BL_TOGG| BL_INC | BL_DEC |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | RESET  |        |        | FN     |        |        | P0     | P0     | P.     | PENT   |        | FN     |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
  [_FN] = { /* FUNCTION */
   { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
-  { KC_TRNS, KC_CALC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
-  { KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
-  { KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_TRNS, KC_CALC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, KC_TRNS },
+  { KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS },
+  { KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_TRNS, BL_TOGG, BL_INC,  BL_DEC,  KC_TRNS, KC_TRNS },
   { RESET,   KC_TRNS, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_KP_0, KC_KP_0, KC_PDOT, KC_PENT, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_TRNS },
  }
 };
