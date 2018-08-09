24d994f21d41ff2a58ce965af3e3b186c60b63eb
Valentin Brosseau <c4software@gmail.com>
2018-08-09 19:19:49 +0200

    Modification du layout pour rendre plus accessible les touches centrales

--- layout/c4software_bepo/qmk_layout/rules.mk
+++ layout/c4software_bepo/qmk_layout/rules.mk
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-MOUSEKEY_ENABLE = yes
-
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
 endif
